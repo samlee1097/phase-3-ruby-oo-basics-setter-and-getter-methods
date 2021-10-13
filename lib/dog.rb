ecd8cd95cf09966c424d36c3464d44da54da70e7
Samuel <samlee1097@yahoo.com>
2021-10-13 19:41:44 -0400

    commit tree 15feeb0b9b6cfe67057c5940b2a1808fca1c32d5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(value)
+        @name = value
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(type)
+        @breed = type
+    end
+
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+
+    def job=(type)
+        @job = type
+    end
+
+    def job
+        @job
+    end
 
 end
